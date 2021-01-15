f94b6da05e8aad7f6514b7faafa50ae3534859d3
levilu800b <levilu800b@gmail.com>
2021-01-15 20:17:36 +0000

    yes

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,7 @@
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
-export const UPDATE_TODO = "UPDATE_TODO"
+export const UPDATE_TODO = "UPDATE_TODO";
+
 
 export function addTodo(todo) {
     return {
@@ -22,3 +23,6 @@ export function updateTodo(todo) {
         payload: todo,
     }
 }
+
+
+
